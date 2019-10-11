6e3153d1db30914211b74c077c04ff644b72cd9d
Emily Costa <emilys2costa@gmail.com>
2019-10-11 16:43:04 -0300

    Script para criar pastar user

---
+++ criarPastaUser.cpp
@@ -0,0 +1,16 @@
+bool isLoggedIn(){
+	string username, password, un, pw;
+
+	cout << "Entre com nome de usuario: "<<endl;
+	cin >> username;
+
+	cout << "Entre com a senha: "<<endl;
+	cin >> password;
+
+	ofstream read;
+		read.open("data\\" + username + ".txt");
+		getline(read, username);
+		getline(read, password);
+
+	read.close();
+}
